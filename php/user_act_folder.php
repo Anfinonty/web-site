e3ecc843968c2ea1aa77afad02c530234b4f243f
Anfinonty <97777854+Anfinonty@users.noreply.github.com>
2023-07-18 16:27:09 +0800

    Update user_act_folder.php
    add .zip uploads

--- php/user_act_folder.php
+++ php/user_act_folder.php
@@ -70,7 +70,7 @@
   </form>
   <br>
   <?php
-    $extensions= array("html","js","css","txt","mp3","flac","wav","gif","png","apng","bmp","jpg","jpeg","mov","mp4","mkv");
+    $extensions= array("html","js","css","txt","mp3","flac","wav","gif","png","apng","bmp","jpg","jpeg","mov","mp4","mkv","zip");
     echo "Files Allowed: ";
     for ($i=0;$i<sizeof($extensions);$i++) {
       echo $extensions[$i].", ";
