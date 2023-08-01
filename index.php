1cadeeb3ec79360995539be380043ac2ae15cdbb
Anfinonty <97777854+Anfinonty@users.noreply.github.com>
2023-08-01 22:33:48 +0800

    07-31-2023 Added comments to index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php include $_SERVER['DOCUMENT_ROOT']."/php/user_header.php"; ?>
-
 <html>
+
+
+<!-- Head -->
 <head>
   <!--<title>G'day M8! Welcome To My Web-site :D</title>-->
   <title>G'day M8! :D</title>
@@ -14,20 +16,31 @@
   </style>
 </head>
 
+
+
+
+<!-- Body -->
 <body>
+
+
+
+  <!-- Touchscreen Buttons For Movement of Live Peerrs Avatar -->
   <div id="axis_click_buttons" hidden="">
   <form method="post" target="nfresh" style="display:inline;">
-    <?php 
-      for ($y=0;$y<$GR_HEIGHT;$y+=$CURSOR_GAP) {
+    <?php       
+      /*for ($y=0;$y<$GR_HEIGHT;$y+=$CURSOR_GAP) {
         for ($x=0;$x<$GR_WIDTH;$x+=$CURSOR_GAP) {
           $name='btnAxisClick'."_x".$x."_y".$y;
           echo '<input type="submit" id='.$name,' name='.$name.' value="." onmouseover="afk_timer=0;this.click();">';
         }
-      }
+      }*/
     ?>
   </form>
   </div>
 
+
+
+  <!-- Webpage @ Greeting -->
   <div id="default_chatroom_greet">
   <h1>G'day M8!</h1>
     <?php
@@ -38,8 +51,15 @@
     <br>Say Something or Hear from Other Peers! <br>
   </div>
 
+
+
+  <!-- For Page Refresh Without Page Reload -->
   <iframe name="nfresh" style="display:none;"></iframe>
   <div id="users_onscreen" hidden></div>
+
+
+
+  <!-- Buttons For Actions Update Chat, Flip Chat, Live Chat and Live Peers -->
   <span id="chatbox_actions">
     <button onclick="UpdateChat(1,0)">Hear</button>
     <button onclick="ViewOrder(0,1,flip,document.getElementById('txthere').textContent,0)">Flip</button>
@@ -48,12 +68,16 @@
     <button id="cleartxt" onclick="ClearTxtButton()">Clear</button>
   </span>
 
+
+
+  <!-- OnScreen Buttons For Peers Avata Movement -->
   <form method="post" id="formaxis" name="formaxis" target="nfresh" style="display:inline;">
     <input hidden type="submit" id="btnXMinus" name="btnXMinus" onmouseover="afk_timer=0;this.click();" value="<">
     <input hidden type="submit" id="btnYMinus" name="btnYMinus" onmouseover="afk_timer=0;this.click();" value="^">
     <input hidden type="submit" id="btnYPlus" name="btnYPlus" onmouseover="afk_timer=0;this.click();" value="V">
     <input hidden type="submit" id="btnXPlus" name="btnXPlus" onmouseover="afk_timer=0;this.click();" value=">">
   </form>
+
 
   <form method="post" id="form1" name="form1" target="nfresh">
     <input type="submit" id="btnSubmit" name="btnSubmit" value="Say Something" onclick="SubmitMsg(this)"><br>
@@ -71,8 +95,10 @@
       $user_post_msg=ChatShortcuts($user_post_msg);
 
       $txt_user_ip_address=GetUsernameFromIpAddress($USER_IP_ADDRESS);
-      if (file_exists($SELF_USER_FOLDER_NAME)) {$txt_user_ip_address="/".GetUsernameFromIpAddress($USER_IP_ADDRESS);}
-      $user_msg="[$date_time_now][$txt_user_ip_address] $user_post_msg #e#\n";//ensures no incomplete html overflow
+      if (file_exists($SELF_USER_FOLDER_NAME)) {
+	$txt_user_ip_address="/".GetUsernameFromIpAddress($USER_IP_ADDRESS);
+      }
+      $user_msg="[$date_time_now][$txt_user_ip_address] $user_post_msg #e#\r\n";//ensures no incomplete html overflow
 
       if (strlen($user_post_msg)>0) {//not blank
         $fp=fopen($CHAT_DIR,"a"); //open in append mode
@@ -88,12 +114,17 @@
 //
 //
   ?>
+
+
   <div style='display:inline;margin:0;' id='timeUTC'></div>
   <div id='chathere' height:400px;>
     <?php
       echo htmlspecialchars(strval(file_get_contents($_SERVER['DOCUMENT_ROOT']."/".$CHAT_DIR)));
     ?>
   </div>
+
+
+
   <?php
 //AXIS MOVEMENT
     if (isset($_POST['btnXPlus']) || 
@@ -108,10 +139,11 @@
         $p_y=$axis_arr[1];
         $time_expire=$axis_arr[2];
         $p_name=GetUsernameFromIpAddress($USER_IP_ADDRESS);
-        if (isset($_POST['btnXPlus']) && $p_x<$GR_WIDTH) {$p_x=$p_x+"10";}
-        if (isset($_POST['btnXMinus']) && $p_x>0) {$p_x=$p_x-"10";}
-        if (isset($_POST['btnYMinus']) && $p_y>0) {$p_y=$p_y-"10";}
-        if (isset($_POST['btnYPlus']) && $p_y<$GR_HEIGHT) {$p_y=$p_y+"10";}
+        if (isset($_POST['btnXPlus']) && $p_x<$GR_WIDTH) {$p_x=$p_x+"32";}
+        if (isset($_POST['btnXMinus']) && $p_x>0) {$p_x=$p_x-"32";}
+        if (isset($_POST['btnYMinus']) && $p_y>0) {$p_y=$p_y-"32";}
+        if (isset($_POST['btnYPlus']) && $p_y<$GR_HEIGHT) {$p_y=$p_y+"32";}
+
         //Write to own file
         $fp=fopen($SELF_USER_SESSION,"w");
         fwrite($fp, $p_x.",".$p_y.",".$time_expire.",".$p_name);
@@ -124,8 +156,11 @@
         fclose($fp);
       }
     }
-//Cursor!!
-  for ($y=0;$y<$GR_HEIGHT;$y+=$CURSOR_GAP) {
+
+
+
+//Cursor!! (Legacy)
+  /*for ($y=0;$y<$GR_HEIGHT;$y+=$CURSOR_GAP) {
     for ($x=0;$x<$GR_WIDTH;$x+=$CURSOR_GAP) {
       if (isset($_POST['btnAxisClick'.'_x'.$x.'_y'.$y])) {
         $axis=htmlspecialchars(strval(file_get_contents($SELF_USER_SESSION)));
@@ -145,7 +180,9 @@
         }
       }
     }
-  }
+  }*/
   ?>
+
 </body>
 </html>
+
