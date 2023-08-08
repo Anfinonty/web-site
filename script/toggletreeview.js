394b5bd5c5cfa36a4112578cbd866502e663621b
Anfinonty <97777854+Anfinonty@users.noreply.github.com>
2023-08-08 23:20:44 +0800

    Update toggletreeview.js
    new capabilities: website now shows , if index.html exists, when folder in site-content is opened.

--- script/toggletreeview.js
+++ script/toggletreeview.js
@@ -4,20 +4,37 @@
     e = document.getElementById(_e); //folder directory
     e2=document.getElementById(_e.concat("_branch"));
     e3=document.getElementById(_e.concat("_button"));
+    e4=document.getElementById(_e.concat("_folder_tab"));
+    e5=document.getElementById(_e.concat("_last_branch"));
     const folder_files=document.getElementsByClassName(_e);
     const folder_video_files=document.getElementsByClassName(_e.concat("_video"));
     const folder_audio_files=document.getElementsByClassName(_e.concat("_audio"));
+
     _dvd="/dvd/AVRIL_LAVIGNE";
     if (e.style.display=="none") { //open
-      e.style.display="block"; //folder open
+      e.style.display="block"; //folder open, causes the break
       e.style.borderWidth="thick"; //folder border size
       e.style.borderStyle="solid"; //folder border type
+
+      e3.style.display="inline-table"; //button break
+      e3.style.borderWidth="thick"; //button border size
+      e3.style.borderStyle="solid"; //button border type
+
       if (_e!=_dvd) {
-        e.style.marginBottom="32px"; //folder bottom margin
+        //e.style.marginBottom="32px"; //folder bottom margin
+        //e3.style.marginTop="32px"; //button margin top open
+
         e2.style.display="inline-table"; //branch in button open
-        e3.style.marginTop="32px"; //button margin top open
+        e5.style.display="inline-table"; //final branch open
+
+        try {
+	  e4.style.display="block";
+	  e3.style.display="block";
+          e5.style.display="block"; //final branch open
+	  e4.style.width="100%";
+	  e4.style.height="100%";
+	} catch (error) {}; //folder tab open
       }
-      e3.style.display="block"; //button
       for (i=0;i<folder_files.length;i++) {	//open branch class folder
 	folder_files[i].src=folder_files[i].id;
       }
@@ -31,7 +48,14 @@
       e.style.display="none"; // folder close
       e2.style.display="none"; //branch in button close
       e3.style.display="inline-table"; //button snap back
-      e3.style.marginTop="0px"; //button margin top close
+      e3.style.borderWidth="none";
+      e3.style.borderStyle="none";
+
+      try {e4.style.display="none";} catch (error) {} //iframe in button close
+      e5.style.display="none"; //branch in button close
+
+
+      //e3.style.marginTop="0px"; //button margin top close
       /*for (i=0;i<folder_files.length;i++) {	//close branch class folder
 	folder_files[i].src="";
       }*/
